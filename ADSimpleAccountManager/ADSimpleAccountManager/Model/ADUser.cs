41817befd5eb1cba2c5c4aafc77947237ea24ac8
axa <axamestegui@gmail.com>
2022-08-15 14:52:55 +0200

    Initial commit

---
+++ ADSimpleAccountManager/ADSimpleAccountManager/Model/ADUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ADSimpleAccountManager.Model
+{
+    internal class ADUser : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string GivenName { get; set; }
+        public string UserPrincipalName { get; set; }
+        public string DistinguishedName { get; set; }   
+        public string EMail { get; set; }
+        public string SN { get; set; }
+        public string Adspath { get; set; }
+        public string Surname { get; set; }
+        public string EmployeeId { get; set; }
+        public string Guid { get; set; }    
+        public string Sid { get; set; }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ ADSimpleAccountManager/ADSimpleAccountManager/Model/AppSw1User.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ADSimpleAccountManager.Model
+{
+    public class AppSw1User : INotifyPropertyChanged
+    {
+        private int keyId;
+        private int refProfile;
+        private int intBarcode;
+        private string txtName = string.Empty;
+        private string txtLogin = string.Empty;
+        private string txtPassword = string.Empty;
+        private byte bolActive;
+        private string txtRemarks = string.Empty;
+        private byte bolMustChangePassworkd;
+
+        public int KeyId { get { return keyId; } }
+        public int Profile
+        {
+            get { return refProfile; }
+            set
+            {
+                if (value != refProfile)
+                {
+                    refProfile = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+        public int Barcode
+        {
+            get { return intBarcode; }
+            set
+            {
+                if (value != intBarcode)
+                {
+                    intBarcode = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+        public string Name
+        {
+            get { return txtName; }
+            set
+            {
+                if (value != txtName)
+                {
+                    txtName = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+        public string Login
+        {
+            get { return txtLogin; }
+            set
+            {
+                if (value != txtLogin)
+                {
+                    txtLogin = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+        public string Password
+        {
+            get { return txtPassword; }
+            set
+            {
+                if (value != txtPassword)
+                {
+                    txtPassword = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+        public byte Active { get { return bolActive; }
+            set
+            {
+                if (value != bolActive)
+                {
+                    bolActive = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+        public byte MustChangePassworkd { get { return bolMustChangePassworkd; }
+            set
+            {
+                if (value!= bolMustChangePassworkd)
+                {
+                    bolMustChangePassworkd = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+        public string Remarks
+        {
+            get { return txtRemarks; }
+            set
+            {
+                if (value != txtRemarks)
+                {
+                    txtRemarks = value;
+                    INotifyPropertyChanged();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{refProfile} {txtName}";
+        }
+
+        #region INotifyPropertyChanged Members
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void INotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
