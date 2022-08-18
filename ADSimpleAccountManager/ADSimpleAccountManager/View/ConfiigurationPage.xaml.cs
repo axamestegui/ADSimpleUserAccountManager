e0829651a9680ee9e3e8eeb92df003ba5e0e9870
axa <axamestegui@gmail.com>
2022-08-18 07:48:26 +0200

    #0001 wip commiting documentations updates.

---
+++ ADSimpleAccountManager/ADSimpleAccountManager/View/ConfiigurationPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADSimpleAccountManager.View
+{
+    /// <summary>
+    /// Interaction logic for ConfiigurationPage.xaml
+    /// </summary>
+    public partial class ConfiigurationPage : Page
+    {
+        public ConfiigurationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DirectoryServicesSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnGetActiveDirectoryObjects_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- ADSimpleAccountManager/ADSimpleAccountManager/View/MainWindow.xaml.cs
+++ ADSimpleAccountManager/ADSimpleAccountManager/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ADSimpleAccountManager.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -43,7 +44,7 @@ namespace ADSimpleAccountManager
 
         private void ToolConfigurationsClick(object sender, RoutedEventArgs e)
         {
-
+            MainDisplay.Content = new ConfiigurationPage();
         }
 
         private void CompareUsersClick(object sender, RoutedEventArgs e)
